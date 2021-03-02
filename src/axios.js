80e0c87a69385dc143fb2dbdfa2128edd4405d46
Namdev <akashjadhavafi41@gmail.com>
2021-03-02 15:02:09 +0530

    want to check clone changes can be reflect into the master repo

--- src/axios.js
+++ src/axios.js
@@ -1,9 +1,9 @@
 //import axios from "axios"
 //error due to axios
 
-const instance = axios.create({
+//const instance = axios.create({
 
-    baseURL:"https://api.themoviedb.org/3",
+    //baseURL:"https://api.themoviedb.org/3",
 
-});
-export default instance;
+//});
+//export default instance;
